c130a7022be5446e19912c802349db7997a28f14
Xiaofei Cao <xc9pd@mst.edu>
2017-11-26 14:48:53 -0600

    v1

--- README.md
+++ README.md
@@ -1,9 +1,24 @@
-dsafdsafd
-
-fd
-sg
-fds
-g
-fdds
-g
-fddsgfsdg
+How to use this tools:
+Requirements: (if you don't have python and pywin32 installed in your computer look at bottom)
+    windows
+    python 3.6 and up
+    pywin32
+    
+How to run:
+    1. Go to the folder where you download the 'win32MouseRecorder.py'
+    2. click 'File' button on the top left corner. Open command line.
+    3. In the command line, type the following command between the quote "python ./win32MouseRecorder.py" 
+    4. When you are ready to record, type '1' in your keyboard will start recording. type '1' again will stop recording
+    5. When you finished recording, type '2' in your keyboard will start replay your recorder. Type '2' again will stop replay
+    6. After stop recording, type '3' will stop the program.
+    
+    
+Install python 3.6: go to the following website and download python any version equal or larger than 3.6.3
+                    https://www.python.org/downloads/
+                    run the .exe file you just downloaded. Choose add python to your computer when installation.
+                    Choose add python to your computer when installation. Choose add python to your computer when installation.
+                    Choose add python to your computer when installation.
+                    
+Install pywin32: After you have installed the python 3.6. Go to the following website and download 'pywin32-221.win32-py3.6.exe':
+                https://sourceforge.net/projects/pywin32/files/pywin32/Build%20221/
+                Download 'pywin32-221.win32-py3.6.exe' then run and install it. 
---
+++ win32MouseRecorder.py
@@ -0,0 +1,117 @@
+# Code to check if left or right mouse buttons were pressed
+import win32api
+import win32con
+import time
+
+mouseDown=2
+mouseUp=1
+move=0
+
+def record(ST=0.01):
+    f=open('record.txt','w')
+    f.close()
+    f=open('record.txt','a')
+    leftMouse=0x01
+    rightMouse=0x02
+  
+    state_left = win32api.GetKeyState(leftMouse)  # Left button down = 0 or 1. Button up = -127 or -128
+    state_right = win32api.GetKeyState(rightMouse)  # Right button down = 0 or 1. Button up = -127 or -128
+    record_button = win32api.GetKeyState(0x31)
+    
+    currentTime=time.time()
+    #f.close()
+    while True:
+        a = win32api.GetKeyState(leftMouse)
+        b = win32api.GetKeyState(rightMouse)
+        c = win32api.GetKeyState(0x31)
+        
+        if c != record_button:
+            time.sleep(0.5)
+            f.close()
+            return
+        
+        if time.time() - currentTime > ST:
+            h_x, h_y = win32api.GetCursorPos()
+            #f=open('record.txt','a')
+            f.write("%.3f " % (time.time()-currentTime))
+            f.write(' 0'  + ' ' + str(move) + ' ' + str(h_x) + ' ' + str(h_y) + '\n') 
+            currentTime=time.time()
+        
+        if a != state_left:  # Button state changed
+            state_left = a
+            h_x, h_y = win32api.GetCursorPos()
+            if a < 0:
+                f.write("%.3f " % (time.time()-currentTime))
+                f.write(' ' + str(leftMouse) + ' ' + str(mouseDown) + ' ' + str(h_x) + ' ' + str(h_y) + '\n') 
+            else:
+                f.write("%.3f " % (time.time()-currentTime))
+                f.write(' ' + str(leftMouse) + ' ' + str(mouseUp) + ' ' + str(h_x) + ' ' + str(h_y) + '\n') 
+            currentTime=time.time()    
+
+        if b != state_right:  # Button state changed
+            state_right = b
+            h_x, h_y = win32api.GetCursorPos()
+            if b < 0:
+                f.write("%.3f " % (time.time()-currentTime))
+                f.write(' ' + str(rightMouse) + ' ' + str(mouseDown) + ' ' + str(h_x) + ' ' + str(h_y) + '\n') 
+            else:
+                f.write("%.3f " % (time.time()-currentTime))
+                f.write(' ' + str(rightMouse) + ' ' + str(mouseUp) + ' ' + str(h_x) + ' ' + str(h_y) + '\n') 
+            currentTime=time.time() 
+        time.sleep(0.001)
+        
+def play(loopTime = -1, sleepTime = 0.1):
+    f = open('record.txt','r')
+    lineStr = f.readlines()
+    play_button = win32api.GetKeyState(0x32)
+    while loopTime!=0 :
+        loopTime = loopTime - 1
+        for str in lineStr:
+            currentEvent = str.split()
+            c = win32api.GetKeyState(0x32)
+            if c != play_button:
+                time.sleep(0.5)
+                f.close()
+                return
+                
+            time.sleep(float(currentEvent[0]))
+            if int(currentEvent[1])==0 :
+                win32api.mouse_event(win32con.MOUSEEVENTF_MOVE | win32con.MOUSEEVENTF_ABSOLUTE,
+                int(int(currentEvent[3])/2560*65535),int(int(currentEvent[4])/1080*65535),0,0)
+            if int(currentEvent[1])==1 :
+                win32api.mouse_event(win32con.MOUSEEVENTF_MOVE | win32con.MOUSEEVENTF_ABSOLUTE,
+                int(int(currentEvent[3])/2560*65535),int(int(currentEvent[4])/1080*65535),0,0)
+                if int(currentEvent[2])==mouseDown:
+                    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN | win32con.MOUSEEVENTF_ABSOLUTE,
+                    int(int(currentEvent[3])/2560*65535),int(int(currentEvent[4])/1080*65535),0,0)
+                    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_ABSOLUTE,
+                    int(int(currentEvent[3])/2560*65535),int(int(currentEvent[4])/1080*65535),0,0)
+                if int(currentEvent[2])==mouseUp :
+                    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_ABSOLUTE,
+                    int(int(currentEvent[3])/2560*65535),int(int(currentEvent[4])/1080*65535),0,0)
+        time.sleep(sleepTime)
+
+def _main():
+    while True:
+        toggled = False
+        isPlay = False
+        
+        while win32api.GetKeyState(0x33) not in [0,1]:
+            return
+        
+        while win32api.GetKeyState(0x31) not in [0,1]:
+            toggled = True
+            time.sleep(0.01)
+        if toggled:
+            toggled = False
+            record()
+            
+        while win32api.GetKeyState(0x32) not in [0,1]:
+            isPlay = True
+            time.sleep(0.01)
+        if isPlay:
+            isPlay = False
+            play()
+        time.sleep(0.001)               
+
+_main()
